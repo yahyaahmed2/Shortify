feef5ea10dc1f8d9c975cf22d75a82f01d124267
Yahya Ahmed <yahya.selim2024@gmail.com>
2026-01-15 19:09:25 +0200

    Skeleton + dependencies + MongoDB connection working

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected! "))
+.catch(err => console.log("MongoDB connection error: ", err));
+
+const PORT = process.env.PORT | 5000;
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
