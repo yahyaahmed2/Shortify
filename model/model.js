1f51235786c8db0e93c3efd4d194aaa759c26c09
Yahya Ahmed <yahya.selim2024@gmail.com>
2026-01-16 21:39:45 +0200

    Added model

---
+++ model/model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+    longUrl: { type: String, required: true },
+    shortCode: { type: String, required: true, unique: true },
+    clicks: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+    expiryDate: { type: Date }
+});
+
+module.exports = mongoose.model('Url', urlSchema);
