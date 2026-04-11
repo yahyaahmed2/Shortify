34a5e7e6c89e23793fbea32195e5d0e9ce02fbad
Yahya Ahmed <yahyaselim2020@gmail.com>
2026-04-11 17:54:54 +0200

    Rate limiting for auth, and global

--- routes/shortUrl.js
+++ routes/shortUrl.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
 const { createShortUrl, redirectUrl, getUrlStats } = require('../controller/shortUrlController');
 const {signUp, login} = require('../controller/userController');
+
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: { message: "Too many auth attempts" }
+});
+
+
 router.post('/shorten', createShortUrl);
-router.post('/signup', signUp);
-router.get('/:shortCode', redirectUrl);
+router.post('/signup', authLimiter, signUp);
 router.get('/stats/:shortCode', getUrlStats);
-router.post('/login', login)
+router.get('/:shortCode', redirectUrl);
+router.post('/login', authLimiter, login)
 
 module.exports = router;
